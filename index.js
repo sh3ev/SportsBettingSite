d541837758cc09f8a4f8d9818e313eb36869bb64
Przemysław <przemyslaw.florczyk@gmail.com>
2020-01-02 02:38:21 +0100

    6

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
 const config = require('config');
-const MongoClient = require("mongodb");
-const Joi = require("joi");
-const mongoose = require("mongoose");
-const express = require("express");
-const users = require("./routes/users");
-const leauges = require("./routes/leagues");
-const lobbies = require("./routes/lobbies");
-const fixtures = require("./routes/fixtures");
-const authRoute = require("./routes/auth");
+const MongoClient = require('mongodb');
+const Joi = require('joi');
+const mongoose = require('mongoose');
+const express = require('express');
+const users = require('./routes/users');
+const leauges = require('./routes/leagues');
+const lobbies = require('./routes/lobbies');
+const fixtures = require('./routes/fixtures');
+const authRoute = require('./routes/auth');
 const app = express();
-var cors = require('cors')
+// var cors = require('cors')
 require('./routes/prod')(app);
 
 if (!config.get('jwtPrivateKey')) {
-	console.error('FATAL ERROR: jwtPrivateKey is not defined');
-	process.exit(1)
+  console.error('FATAL ERROR: jwtPrivateKey is not defined');
+  process.exit(1);
 } else if (!config.get('ApiKey')) {
-	console.error('FATAL ERROR: Api Key is not defined');
-	process.exit(1)
+  console.error('FATAL ERROR: Api Key is not defined');
+  process.exit(1);
 }
 
-
-app.use(cors())
-app.use(
-	express.urlencoded({
-		extended: true
-	})
-);
+// app.use(cors())
+// app.use(
+// 	express.urlencoded({
+// 		extended: true
+// 	})
+// );
 app.use(express.json());
-app.use("/api/user", authRoute);
-app.use("/api/users", users);
-app.use("/api/lobbies", lobbies);
-app.use("/api/leagues", leauges);
-app.use("/api/fixtures", fixtures);
+app.use('/api/user', authRoute);
+app.use('/api/users', users);
+app.use('/api/lobbies', lobbies);
+app.use('/api/leagues', leauges);
+app.use('/api/fixtures', fixtures);
 
 // DB CONNECTION
 mongoose
-	.connect(
-		"mongodb+srv://przemek:1234@cluster0-pwyj2.mongodb.net/test?retryWrites=true&w=majority", {
-			useNewUrlParser: true,
-			useUnifiedTopology: true
-		}
-	)
-	.then(() => console.log("Connected to MongoDB..."))
-	.catch(err => console.error("Could not connect to MongoDB..."));
+  .connect(
+    'mongodb+srv://przemek:1234@cluster0-pwyj2.mongodb.net/test?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  )
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
 
 // START LISTETING TO THE SERVER
 const port = process.env.PORT || 3001;
